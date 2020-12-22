259875e1ca23a55d817fdbb8a78f6bc9005bd514
EC2 Default User <ec2-user@ip-172-31-31-187.ec2.internal>
2020-12-22 03:31:29 +0000

    [add]admin genreコントローラ

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,2 +1,39 @@
-class Admin::GenresController < ApplicationController
+class Admins::GenresController < ApplicationController
+  before_action :authenticate_admin!
+
+  def index
+    @genre = Genre.new
+    @genres = Genre.page(params[:page]).per(10)
+  end
+
+  def create
+    @genre = Genre.new(genre_params)
+    if @genre.save
+      flash[:notice] = "ジャンルを登録しました"
+      redirect_to admins_genres_path
+    else
+      @genres = Genre.page(params[:page]).per(10)
+      render 'index'
+    end
+  end
+
+  def edit
+    @genre = Genre.find(params[:id])
+  end
+
+  def update
+    @genre = Genre.find(params[:id])
+    if @genre.update(genre_params)
+      flash[:notice] = "ジャンル情報を更新しました"
+      redirect_to admins_genres_path
+    else
+      render 'edit'
+    end
+  end
+
+  private
+  def genre_params
+    params.require(:genre).permit(:name, :is_active)
+  end
+
 end
